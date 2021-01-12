b1de22780f3dacad2220389226e5dad20665734b
Melissa Ryan <melissaryandigital@gmail.com>
2021-01-11 22:53:19 -0600

    Begin setting up schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id integer primary key,
+  msg text not null,
+  userId integer,
+  FOREIGN KEY(userId) REFERENCES users(id),
+  roomId integer,
+  FOREIGN KEY(roomId) REFERENCES rooms(id)
+);
+
+CREATE TABLE users (
+  id integer primary key,
+  username text not null
+);
+
+CREATE TABLE rooms (
+  id integer primary key,
+  roomName text not null
 );
 
 /* Create other tables and define schemas for them here! */
